15b834f6f850c3796ac01337f4a0cbcbd9abf575
Martin Eberle <code@martineberle.io>
2024-02-11 09:56:25 -0500

    Housekeeping.

---
+++ custom_components/announce-bot/const.py
@@ -0,0 +1,9 @@
+"""Constants for announce-bot."""
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "Announce Bot"
+DOMAIN = "announce-bot"
+VERSION = "0.0.0"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
